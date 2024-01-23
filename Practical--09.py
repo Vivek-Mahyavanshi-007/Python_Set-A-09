8ff02bb08e2d6650b02a3229aaa6422969e41abb
Vivek-Mahyavanshi-007 <157041563+Vivek-Mahyavanshi-007@users.noreply.github.com>
2024-01-23 21:00:31 +0530

    Add files via upload

---
+++ Practical--09.py
@@ -0,0 +1,12 @@
+fh=open('Output.txt','w')
+
+n=int(input("Enter the number: "))
+
+i=1
+while i <= 10 :
+    print(n ,"*",i,"=",n*i)
+    fh.write(str(n)+" * "+str(i)+" = "+str(n*i))
+    fh.write('\n')
+    i=i+1
+
+fh.close()
